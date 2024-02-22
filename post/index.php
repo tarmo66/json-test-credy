cf33d8b8604937a79440811d9a32f114175c4083
tarmo66 <tarmo@traktorist.org>
2024-02-22 16:27:01 +0200

    Update index.php

--- post/index.php
+++ post/index.php
@@ -4,14 +4,14 @@ $signature = sha1($time . 'credy');
 
 $json = '{
 "data":[
-  {"first_name":"Tarmo", "last_name":"Allikvee", "email":"tarmo@traktorist.org", "bio":"A PHP developer with e-commerce experience is looking for web development projects that bring business results to the customer.", "technologies":"JSON, Git, JavaScript, PHP, MySQL, SOAP API, REST API, XML, CSS Adobe Photoshop, Adobe Illustrator, Adobe Flash, Adobe After Effects, Bootstrap, Wordpress, Woocommerce, Codeigniter, Visual Basic, C-Panel", "timestamp":"' . $time . '", "signature":"' . $signature . '", "vcs_uri":"https://github.com/tarmo66/json-test-credy"}
+  {"first_name":"Tarmo", "last_name":"Allikvee", "email":"tarmo@traktorist.org", "bio":"A PHP developer with e-commerce experience is looking web development projects that bring business results to the customer.", "technologies":"JSON, Git, JavaScript, PHP, MySQL, SOAP API, REST API, XML, CSS Adobe Photoshop, Adobe Illustrator, Adobe Flash, Adobe After Effects, Bootstrap, Wordpress, Woocommerce, Codeigniter, Visual Basic, C-Panel", "timestamp":"' . $time . '", "signature":"' . $signature . '", "vcs_uri":"https://github.com/tarmo66"}
 ]
 }';
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://cv.microservices.credy.com/v1',
+  CURLOPT_URL => 'https://www.traktorist.org/traffic/read.php',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
